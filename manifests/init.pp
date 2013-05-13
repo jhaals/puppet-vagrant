d28ca9badc47db2d57141c85efe77644c4f31e63
Will Farrington <wfarr@github.com>
2013-05-13 16:37:10 -0700

    Upgrade to install 1.2.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-# Public: Installs Vagrant 1.2.1
+# Public: Installs Vagrant 1.2.2
 #
 # Usage:
 #
@@ -7,7 +7,7 @@
 class vagrant {
   package { 'Vagrant':
     ensure   => installed,
-    source   => 'http://files.vagrantup.com/packages/a7853fe7b7f08dbedbc934eb9230d33be6bf746f/Vagrant-1.2.1.dmg',
+    source   => 'http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/Vagrant-1.2.2.dmg',
     provider => 'pkgdmg'
   }
 
