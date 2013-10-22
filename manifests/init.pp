7c65b207c4d8527403aa77c1ec84d2de72af6f0b
Izzet Mustafaiev <webdizz@gmail.com>
2013-10-22 16:03:22 +0300

    Update to 1.3.5

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@
 class vagrant {
   package { 'Vagrant':
     ensure   => installed,
-    source   => 'http://files.vagrantup.com/packages/0ac2a87388419b989c3c0d0318cc97df3b0ed27d/Vagrant-1.3.4.dmg',
+    source   => 'http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant-1.3.5.dmg',
     provider => 'pkgdmg'
   }
 
