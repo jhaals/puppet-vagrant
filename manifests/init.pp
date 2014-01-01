f1c57584694ceb77523bd9c6296eec706f5cc17e
Tony R Quilkey <trq@thorpesystems.com>
2014-01-01 15:54:17 +1100

    Bump version to 1.4.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
-# Public: Installs Vagrant 1.4.1
+# Public: Installs Vagrant 1.4.2
 #
 # Usage:
 #
 #   include vagrant
 
 class vagrant {
-  package { 'Vagrant_1_4_1':
+  package { 'Vagrant_1_4_2':
     ensure   => installed,
-    source   => 'https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.1.dmg',
+    source   => 'https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.2.dmg',
     provider => 'pkgdmg'
   }
 
