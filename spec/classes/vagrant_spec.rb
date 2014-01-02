1a3479f5e2517d41b1e0644a855d0b4fb30d3afc
Tony R Quilkey <trq@thorpesystems.com>
2014-01-02 11:27:38 +1100

    Update the spec

--- spec/classes/vagrant_spec.rb
+++ spec/classes/vagrant_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'vagrant' do
   it do
-    should contain_package('Vagrant_1_4_1').with({
+    should contain_package('Vagrant_1_4_2').with({
       :ensure   => 'installed',
       :provider => 'pkgdmg'
     })
