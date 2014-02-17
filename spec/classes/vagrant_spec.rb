7b1cc9fba5f96324da7f9921a02704a771f14bc9
stack72 <public@paulstack.co.uk>
2014-02-17 15:56:01 +0000

    Fixing specs to make sure that 1.4.2 is the default to go with with is in upstream master

--- spec/classes/vagrant_spec.rb
+++ spec/classes/vagrant_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe 'vagrant' do
   it do
-    should contain_package('Vagrant_1_4_3').with({
+    should contain_package('Vagrant_1_4_2').with({
       :ensure   => 'installed',
       :provider => 'pkgdmg'
     })
   end
 
   context 'when specifying a specific version' do
-    let (:params) {{:version => '1.4.2'}}
+    let (:params) {{:version => '1.4.3'}}
 
-    it should contain_package('Vagrant_1_4_2').with_version('1.4.2')
-    it should contain_package('Vagrant_1_4_2').with_source('https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.2.dmg')
+    it should contain_package('Vagrant_1_4_3').with_version('1.4.3')
+    it should contain_package('Vagrant_1_4_3').with_source('https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.3.dmg')
   end
 end
