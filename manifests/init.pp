565559ed2546fe8b5bd72dd5408645f8be6b09bc
Steve Huff <shuff@vecna.org>
2014-04-23 00:38:32 -0400

    enable installation of bash completion
    No change from default behavior, but optionally install
    vagrant-completion homebrew package.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,17 @@
 #
 #   include vagrant
 
-class vagrant($version = '1.6.3') {
+class vagrant(
+  $version = '1.6.3',
+  $completion = false
+) {
+  validate_bool($completion)
+
+  $ensure_pkg = $completion ? {
+    true    => 'present',
+    default => 'absent',
+  }
+
   package { "Vagrant_${version}":
     ensure   => installed,
     source   => "https://dl.bintray.com/mitchellh/vagrant/vagrant_${version}.dmg",
@@ -12,6 +22,15 @@ class vagrant($version = '1.6.3') {
   }
 
   file { "/Users/${::boxen_user}/.vagrant.d":
-    ensure => directory
+    ensure  => directory,
+    require => Package["Vagrant_${version}"],
+  }
+
+  homebrew::tap { 'homebrew/completions': }
+
+  package { 'vagrant-completion':
+    ensure   => $ensure_pkg,
+    provider => 'homebrew',
+    require  => Homebrew::Tab['homebrew/completions'],
   }
 }
--- spec/classes/vagrant_spec.rb
+++ spec/classes/vagrant_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'vagrant' do
+  let (:facts) { default_test_facts }
+
   describe 'when not specifiying a version' do
     it { should contain_package('Vagrant_1.6.3').with({
       :ensure   => 'installed',
@@ -13,6 +15,14 @@ describe 'vagrant' do
 
     it { should contain_package('Vagrant_1.5.0')}
     it { should contain_package('Vagrant_1.5.0').with_source('https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.0.dmg')}
+  end
+
+  describe 'when installing bash completion' do
+    let (:params) {{:completion => true}}
+
+    it { should contain_package('Vagrant_1.6.3')}
+    it { should contain_package('Vagrant_1.6.3').with_source('https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3.dmg')}
+    it { should contain_package('vagrant-completion').with_provider('homebrew')}
   end
 
 end
